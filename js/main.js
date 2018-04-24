5fe4fbacdecbfbdbc332a7ef6c06686d0cc23ca5
Fernando Espinosa Victoriano <fespinosa@fespinosa.nt.core>
2018-04-24 16:09:56 -0400

    add service worker to cache assets

--- js/main.js
+++ js/main.js
@@ -4,6 +4,30 @@ let restaurants,
 var map
 var markers = []
 
+
+/**
+ * Register the service worker
+ */
+registerServiceWorker = () => {
+  if (!navigator.serviceWorker) return;
+
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js')
+      .then(function (registration) {
+        console.log('Service Worker registration successful with scope: ',
+          registration.scope);
+      })
+      .catch(function (err) {
+        console.log('Service Worker registration failed: ', err);
+      });
+  }
+
+
+};
+registerServiceWorker();
+
+
+
 /**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
